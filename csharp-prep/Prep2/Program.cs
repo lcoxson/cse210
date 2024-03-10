b56f04d1badfb253a67db310244d5388cd6163d3
lcoxson <coxson.laura@gmail.com>
2024-03-09 21:19:13 -0700

    Completed Prep 2 - Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        int gradePercent = int.Parse(gradeInput);
+        string letter = "";
+        
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+        
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You didn't pass the class. Don't worry, you can always try again!");
+        }
     }
 }
