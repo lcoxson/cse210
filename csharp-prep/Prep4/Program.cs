a5d3d007141b91c05d8ff826d9161f3ae9578753
lcoxson <coxson.laura@gmail.com>
2024-03-11 23:15:16 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Please enter the number you would like to add to the list. Enter 0 to finish. ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+
+        // Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        // Compute the average
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The average is: {average}");
+
+
+        // Find the maximum
+        int maximum = 0;
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine($"The maximum is: {maximum}");
     }
 }
