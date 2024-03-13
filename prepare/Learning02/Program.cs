09d4c0333e06641e63dbb8ea7c45a84aeaa5b919
lcoxson <coxson.laura@gmail.com>
2024-03-13 09:50:02 -0600

    Completed Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "BYU Pathway";
+        job1._jobTitle = "Administrative Assistant";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+
+        Job job2 = new Job();
+        job2._company = "Disney";
+        job2._jobTitle = "Mickey Mouse";
+        job2._startYear = 1984;
+        job2._endYear = 2084;
+
+
+        Resume mickeyMouse = new Resume();
+        mickeyMouse._name = "Mickey Mouse";
+        mickeyMouse._jobs.Add(job1);
+        mickeyMouse._jobs.Add(job2);
+
+        mickeyMouse.DisplayResume();
     }
 }
